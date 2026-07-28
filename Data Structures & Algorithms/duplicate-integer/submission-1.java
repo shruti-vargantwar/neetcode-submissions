89cac24acd683e30c05a14bd9dddd8468042b585
Shruti Vargantwar <47750477+shruti-vargantwar@users.noreply.github.com>
2026-07-28 12:37:48 -0400

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,19 @@
+/**
+TC: O(n)
+SC: O(n)
+*/
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+
+        return false;
+    }
+}
