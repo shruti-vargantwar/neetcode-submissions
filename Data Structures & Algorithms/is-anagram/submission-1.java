428bb8de910dc2321d2e52b41c1969baf803a1ce
Shruti Vargantwar <47750477+shruti-vargantwar@users.noreply.github.com>
2026-07-28 15:25:48 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,28 @@
+/*
+TC: O(n)
+SC: O(n)
+*/
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        int strLen = s.length();
+
+        for (int i = 0; i < strLen; i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+        }
+
+        for (char c : map.keySet()) {
+            if (map.get(c) != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
